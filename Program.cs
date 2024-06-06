9184b2365d7bbcc04bdbe7e8dbf274d00dcd8d1a
Christ ASSANA <christassana@gmail.com>
2024-06-06 12:05:53 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] tab1 = { "ce", "wed", "xa" };
+        string[] tab2 = { "the", "wild", "cat"};
+        string[] tab3 = new string[6];
+
+        tab3[0]=tab1[0];
+        tab3[1]=tab1[1];
+        tab3[2]=tab1[2];
+        tab3[3]=tab2[0];
+        tab3[4]=tab2[1];
+        tab3[5]=tab2[2];
+
+    }
+}
